19a11b9cefd5adb569c10ab656da01ff8b30c433
KipchirchirIan <potterke4@gmail.com>
2020-04-20 15:49:16 +0300

    Set error reporting and timezone

---
+++ config/settings.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: Ian Kipchirchir
+ * Date: 4/20/20
+ * Time: 2:47 PM
+ */
+
+// Error reporting for production
+error_reporting(0);
+ini_set('display_errors', '0');
+
+// Timezone
+date_default_timezone_set('Africa/Nairobi');
