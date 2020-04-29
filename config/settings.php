d11d7842e316751db17dd2dbd427b6b032bed883
KipchirchirIan <potterke4@gmail.com>
2020-04-29 03:54:41 +0300

    Refactor settings, add database and environment configs

--- config/settings.php
+++ config/settings.php
@@ -6,8 +6,6 @@
  * Time: 2:47 PM
  */
 
-use DI\Container;
-
 // Error reporting for production
 error_reporting(0);
 ini_set('display_errors', '0');
@@ -15,14 +13,55 @@ ini_set('display_errors', '0');
 // Timezone
 date_default_timezone_set('Africa/Nairobi');
 
+// Settings
+$settings = [];
 
-return function (Container $container) {
-    $container->set('settings', function () {
-        return [
-            'name' => 'Custom Email Signature Generator - REST API',
-            'displayErrorDetails' => true,
-            'logErrorDetails' => true,
-            'logErrors' => false,
-        ];
-    });
-};
+// Path settings
+$settings['root'] = dirname(__DIR__);
+$settings['temp'] = $settings['root'] . '/tmp';
+$settings['public'] = $settings['root'] . '/public';
+
+$settings['app_name'] = 'Custom Email Signature Generator - REST API';
+
+$settings['error_handler_middleware'] = [
+    // Should be set to false in production
+    'displayErrorDetails' => true,
+
+    // Parameter is passed to the default error handler
+    // View in rendered output by enabling the "displayErrorDetails" setting.
+    // For the console and unit tests we also disable it
+    'logErrorDetails' => true,
+
+    // Display error details in log
+    'logErrors' => false,
+];
+
+$settings['db'] = [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'port' => '3306',
+    'database' => 'test',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'flags' => [
+        // Turn off persistent connections
+        PDO::ATTR_PERSISTENT => false,
+        // Enable exceptions
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        // Emulate prepared statements
+        PDO::ATTR_EMULATE_PREPARES => true,
+        // Set default fetch mode to array
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        // Set character set
+        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci'
+    ]
+];
+
+// Load environment configuration
+if (file_exists(__DIR__ . '/../env.php')) {
+    require __DIR__ . '/../env.php';
+} elseif (file_exists(__DIR__ . '/env.php')) {
+    require __DIR__ . '/env.php';
+}
+
+return $settings;
