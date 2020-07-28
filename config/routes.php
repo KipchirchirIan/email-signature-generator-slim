2030374de508a8a138d02432ae7d67259555ba2a
KipchirchirIan <potterke4@gmail.com>
2020-07-28 19:00:05 +0300

    Group routes requiring authentication

--- config/routes.php
+++ config/routes.php
@@ -6,10 +6,13 @@
  * Time: 2:47 PM
  */
 
+use App\Middleware\JwtAuthMiddleware;
 use Slim\App;
 use Slim\Routing\RouteCollectorProxy;
 
 return function(App $app) {
+
+    $app->post('/v1/tokens', \App\Action\Auth\TokenCreateAction::class);
 
     $app->group('/v1', function (RouteCollectorProxy $group) {
 
@@ -19,23 +22,13 @@ return function(App $app) {
 
         $group->post('/users', \App\Action\User\UserCreateAction::class);
 
-        $group->get('/users', \App\Action\User\UserListDataTableAction::class);
-
         $group->get('/users/{id}', \App\Action\User\UserViewAction::class);
 
         $group->put('/users/{id}', \App\Action\User\UserUpdateAction::class);
 
-        $group->delete('/users/{id}', \App\Action\User\UserDeleteAction::class);
-
-        $group->post('/templates', \App\Action\Template\TemplateCreateAction::class);
-
         $group->get('/templates/{id}', \App\Action\Template\TemplateViewAction::class);
 
         $group->get('/templates', \App\Action\Template\TemplateListAction::class);
-
-        $group->delete('/templates/{id}', \App\Action\Template\TemplateDeleteAction::class);
-
-        $group->put('/templates/{id}', \App\Action\Template\TemplateUpdateAction::class);
 
         $group->post('/users/{id}/templates', \App\Action\UserTemplate\UserTemplateCreateAction::class);
 
@@ -49,15 +42,9 @@ return function(App $app) {
 
         $group->put('/users/{id}/images', \App\Action\UserImage\UserImageUpdateAction::class);
 
-        $group->post('/socials', \App\Action\Social\SocialCreateAction::class);
-
         $group->get('/socials', \App\Action\Social\SocialListAction::class);
 
         $group->get('/socials/{id}', \App\Action\Social\SocialViewAction::class);
-
-        $group->put('/socials/{id}', \App\Action\Social\SocialUpdateAction::class);
-
-        $group->delete('/socials/{id}', \App\Action\Social\SocialDeleteAction::class);
 
         $group->post('/users/{id}/socials', \App\Action\UserSocial\UserSocialCreateAction::class);
 
@@ -65,5 +52,25 @@ return function(App $app) {
 
         $group->put('/users/{id}/socials', \App\Action\UserSocial\UserSocialUpdateAction::class);
     });
+
+    $app->group('/v1', function (RouteCollectorProxy $group) {
+
+        $group->delete('/users/{id}', \App\Action\User\UserDeleteAction::class);
+
+        $group->get('/users', \App\Action\User\UserListDataTableAction::class);
+
+        $group->post('/templates', \App\Action\Template\TemplateCreateAction::class);
+
+        $group->delete('/templates/{id}', \App\Action\Template\TemplateDeleteAction::class);
+
+        $group->put('/templates/{id}', \App\Action\Template\TemplateUpdateAction::class);
+
+        $group->post('/socials', \App\Action\Social\SocialCreateAction::class);
+
+        $group->put('/socials/{id}', \App\Action\Social\SocialUpdateAction::class);
+
+        $group->delete('/socials/{id}', \App\Action\Social\SocialDeleteAction::class);
+
+    })->add(JwtAuthMiddleware::class);
 
 };
